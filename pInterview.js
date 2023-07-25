54d04d193a98b385d617cf1736369e09c1b6ef44
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-26 01:47:59 +0600

    programming hero interview quiestion study

--- pInterview.js
+++ pInterview.js
@@ -10,23 +10,45 @@ const timeout = () => {
         clearInterval(interval)
     }
 }
-let interval = setInterval(timeout, 200)
+// let interval = setInterval(timeout, 200)
 
-// const name = "name";
-// const user = {
-//     name: name,
-//     arr: [1, 2, 3],
-// };
 
-// const myName = user;
 
-// myName.arr.push(6);
 
-// myName[name] = "updated Name";
+
+const name = "name";
+const user = {
+    name: name,
+    arr: [1, 2, 3],
+};
+
+const myName = user;
+
+myName.arr.push(6);
+
+myName[name] = "updated Name";
 
 // console.log(myName);
 // console.log(user);
 
+// we can access from access by below way.
+const tanvir = { ami: 'aci', tumi: 'nai' }
+console.log(tanvir['ami']);
 
 
 
+
+
+
+
+// [{name: "A"}, {name: "B"}]
+// age: 10, 20
+// how can I put age in above array  10 and 20;
+
+const array = [{ name: "A" }, { name: "B" }];
+const ages = [10, 20]
+
+for (let i = 0; i < array.length; i++) {
+    array[i].age = ages[i]
+}
+console.log(array);
