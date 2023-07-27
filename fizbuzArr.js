079da13e47d4144aa1676cfd198d3b4c6a09c206
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-27 23:11:01 +0600

    fizbuz with return array

---
+++ fizbuzArr.js
@@ -0,0 +1,29 @@
+function fizzBuzz(n) {
+    if (typeof n !== 'number' || n <= 0 || !Number.isInteger(n)) {
+        throw new Error('Invalid input. Please provide a positive integer.');
+    }
+
+    const result = [];
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            result.push('Fizz');
+        } else if (i % 5 === 0) {
+            result.push('Buzz');
+        } else {
+            result.push(i.toString());
+        }
+    }
+
+    return result;
+}
+
+// Test the function
+try {
+    const output = fizzBuzz(15);
+    console.log(output);
+} catch (error) {
+    console.error(error.message);
+}
