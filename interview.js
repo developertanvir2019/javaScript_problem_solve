426caa2ba32d08aed82223c569cfe4196e9ecea7
developertanvir2019 <developertanvir2019@gmail.com>
2023-09-06 22:44:43 +0600

    remove unnessery code

--- interview.js
+++ interview.js
@@ -1,2 +1,15 @@
-var a = true + true + true * 3;
-console.log(a);
+// let a = true + true + true * 3;
+
+// let a = 1;
+// let b = 0;
+// while (a <= 3) {
+//   a++;
+//   b += a * 2;
+//   console.log(b);
+// }
+
+var a = 10;
+do {
+  a += 1;
+  console.log(a);
+} while (a < 5);
