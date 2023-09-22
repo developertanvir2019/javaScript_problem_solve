006cb6144d9f7188d4fd1c3648c108f153d2c3c4
developertanvir2019 <developertanvir2019@gmail.com>
2023-09-22 22:15:28 +0600

    sorting practice done

--- 10common.js
+++ 10common.js
@@ -7,7 +7,14 @@ const countWord = (p) => {
 
 // countWord(arr);
 
+// problem number 2
+
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 fruits.splice(2, 1, "tanvir", "nibir");
-console.log(fruits);
+// console.log(fruits);
+// এখানে ২ হচ্ছে সেই ইন্ডেক্স নাম্বার যেখান্ থেকে রিমুভ শুরু হবে এবং ১ হচ্ছে কতগুলো রিমুভ হবে।
+
+const sortableArr = [2, 23, 43, 12, 453, 13, 88];
+sortableArr.sort((a, b) => a - b).reverse();
+console.log(sortableArr);
