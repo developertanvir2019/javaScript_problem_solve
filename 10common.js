b09ccb5d55ae4099498db0307ecb612502d602fb
developertanvir2019 <developertanvir2019@gmail.com>
2023-09-22 22:00:32 +0600

    splice explored

--- 10common.js
+++ 10common.js
@@ -5,4 +5,10 @@ const countWord = (p) => {
   });
 };
 
-countWord(arr);
+// countWord(arr);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// At position 2, add 2 elements:
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
