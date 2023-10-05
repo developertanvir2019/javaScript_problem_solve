6f2aff15c9a27c690afb2aef0b1ee1953b6783ff
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-05 20:14:23 +0600

    practice new function

--- pInterview.js
+++ pInterview.js
@@ -1,25 +1,21 @@
 // what is the difference between setInterval and setTimeout ?
 
-//  If we call a function by setTimeOut it call the function once after specific time. 
+//  If we call a function by setTimeOut it call the function once after specific time.
 //  If we call a function by setInterval it call again and again after specific time , but we can stop the rendering  conditionally by clearInterval.
 
 let num = 0;
 const timeout = () => {
-    console.log(num++);
-    if (num > 9) {
-        clearInterval(interval)
-    }
-}
+  console.log(num++);
+  if (num > 9) {
+    clearInterval(interval);
+  }
+};
 // let interval = setInterval(timeout, 200)
-
-
-
-
 
 const name = "name";
 const user = {
-    name: name,
-    arr: [1, 2, 3],
+  name: name,
+  arr: [1, 2, 3],
 };
 
 const myName = user;
@@ -28,27 +24,21 @@ myName.arr.push(6);
 
 myName[name] = "updated Name";
 
-console.log(myName);
-console.log(user);
+// console.log(myName);
+// console.log(user);
 
 // we can access from access by below way.
-const tanvir = { ami: 'aci', tumi: 'nai' }
-console.log(tanvir['ami']);
-
-
-
-
-
-
+const tanvir = { ami: "aci", tumi: "nai" };
+console.log(tanvir["ami"]);
 
 // [{name: "A"}, {name: "B"}]
 // age: 10, 20
 // how can I put age in above array  10 and 20;
 
 const array = [{ name: "A" }, { name: "B" }];
-const ages = [10, 20]
+const ages = [10, 20];
 
 for (let i = 0; i < array.length; i++) {
-    array[i].age = ages[i]
+  array[i].age = ages[i];
 }
 console.log(array);
--- practice1.js
+++ practice1.js
@@ -1,35 +1,33 @@
 const sumOfAll = (arr) => {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 == 0) {
-            sum += arr[i];
-        }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sum += arr[i];
     }
-    return sum;
-}
-// let result = sumOfAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-// console.log(result);
-
-
+  }
+  return sum;
+};
+let result = sumOfAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(result);
 
 // short by a-z;
 
 const sortStrings = (str) => {
-    r = str.map(str => str.toLowerCase());
-    r.sort();
-    return r;
-}
+  r = str.map((str) => str.toLowerCase());
+  r.sort();
+  return r;
+};
 
 // console.log(sortStrings(["banana", "Apple", "cherry", "date", "FIG"]));
 
 // short z-a;
-function sortStrings(arr) {
-    arr = arr.map(str => str.toLowerCase());
-    arr.sort().reverse();
-    return arr;
-}
+// function sortStrings(arr) {
+//   arr = arr.map((str) => str.toLowerCase());
+//   arr.sort().reverse();
+//   return arr;
+// }
 
-
-let short = ["banana", "Apple", "cherry", "date", "FIG"].map(s => s.toLocaleLowerCase()).sort()
-console.log(short);
-
+let short = ["banana", "Apple", "cherry", "date", "FIG"]
+  .map((s) => s.toLocaleLowerCase())
+  .sort();
+// console.log(short);
