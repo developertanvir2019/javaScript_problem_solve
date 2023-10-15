48f95e410ceb3e325b86c7bf3fcf0e3cd040c40d
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-15 23:06:30 +0600

    debounce function practice

--- clouserPromis.js
+++ clouserPromis.js
@@ -1,18 +1,39 @@
-function delay(i) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(i)
-        }, i * 1000);
-    })
-}
+// function delay(i) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(i);
+//     }, i * 1000);
+//   });
+// }
 
-const timer = async (n) => {
-    console.log('start timer');
-    for (let i = 0; i <= n; i++) {
-        const result = await delay(i);
-        console.log(result);
-    }
-    console.log('end timer');
-}
+// const timer = async (n) => {
+//   console.log("start timer");
+//   for (let i = 0; i <= n; i++) {
+//     const result = await delay(i);
+//     console.log(result);
+//   }
+//   console.log("end timer");
+// };
 
-timer(3)
+// timer(3);
+
+// find the second largest number in a array
+
+const secondLarge = (arr) => {
+  let sortDec = arr.sort((a, b) => b - a);
+  return sortDec[1];
+};
+
+let ans = secondLarge([1, 2, 53, 23, 444, 223, 412, 32, 756]);
+console.log(ans);
+
+// another way to write debounce function
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, arguments);
+    }, delay);
+  };
+}
