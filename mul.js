07246718900c53fb407792b54db8015bc5559992
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-25 23:45:11 +0600

    multiple practice complete

--- mul.js
+++ mul.js
@@ -1,4 +1,8 @@
+// const mul = (x) => {
+//     return Math.sqrt(x).toFixed(3)
+// }
+// console.log(mul(36));
 const mul = (x) => {
-    return Math.sqrt(x).toFixed(3)
-}
+  return Math.sqrt(x).toFixed(3);
+};
 console.log(mul(36));
