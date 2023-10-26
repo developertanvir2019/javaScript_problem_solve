ebddf6ddb04f27c967044858956ef97e669e6cfe
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-26 23:51:54 +0600

    sum of arrays elements

--- mul.js
+++ mul.js
@@ -6,3 +6,15 @@ const mul = (x) => {
   return Math.sqrt(x).toFixed(3);
 };
 console.log(mul(36));
+
+// calculate the sum of an array
+
+const calculateSum = (arr) => {
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+console.log(calculateSum([1, 2, 3, 4, 5, 6, 73]));
