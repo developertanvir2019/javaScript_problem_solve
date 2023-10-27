328117db82de7215f700d1f5085ea0b417267626
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-27 23:25:06 +0600

    practice factorial and duplicate number from array

--- checkVowels.js
+++ checkVowels.js
@@ -11,15 +11,35 @@
 // const ss = check('ajfeaiajakd')
 // console.log(ss);
 
-
 const check = (str) => {
-    let vowels = 'aeiouAEIOU';
-    count = 0;
-    for (let st of str) {
-        if (vowels.includes(st)) {
-            count += 1;
-        }
+  let vowels = "aeiouAEIOU";
+  count = 0;
+  for (let st of str) {
+    if (vowels.includes(st)) {
+      count += 1;
     }
-    return count;
-}
+  }
+  return count;
+};
 
+const duplicateNumber = (arr) => {
+  let newNum = [];
+  for (i = 0; i < arr.length; i++) {
+    if (newNum.includes(arr[i])) {
+      return arr[i];
+    } else {
+      newNum.push(arr[i]);
+    }
+  }
+};
+
+// console.log(duplicateNumber([2, 3, 4, 5, 6, 7, 3, 5, 2]));
+
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+console.log(factorial(3));
